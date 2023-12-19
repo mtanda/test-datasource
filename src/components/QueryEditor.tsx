f19ff917ef6bec0c90f2eceb0231f70fab2075fe
Mitsuhiro Tanda <mitsuhiro.tanda@gmail.com>
2023-12-20 02:37:56 +0900

    rewrite query function

--- src/components/QueryEditor.tsx
+++ src/components/QueryEditor.tsx
@@ -2,30 +2,21 @@ import React, { ChangeEvent } from 'react';
 import { InlineField, Input } from '@grafana/ui';
 import { QueryEditorProps } from '@grafana/data';
 import { DataSource } from '../datasource';
-import { MyDataSourceOptions, MyQuery } from '../types';
+import { GoogleCalendarDataSourceOptions, GoogleCalendarQuery } from '../types';
 
-type Props = QueryEditorProps<DataSource, MyQuery, MyDataSourceOptions>;
+type Props = QueryEditorProps<DataSource, GoogleCalendarQuery, GoogleCalendarDataSourceOptions>;
 
 export function QueryEditor({ query, onChange, onRunQuery }: Props) {
-  const onQueryTextChange = (event: ChangeEvent<HTMLInputElement>) => {
-    onChange({ ...query, queryText: event.target.value });
+  const onCalendarIdChange = (event: ChangeEvent<HTMLInputElement>) => {
+    onChange({ ...query, calendarId: event.target.value });
   };
 
-  const onConstantChange = (event: ChangeEvent<HTMLInputElement>) => {
-    onChange({ ...query, constant: parseFloat(event.target.value) });
-    // executes the query
-    onRunQuery();
-  };
-
-  const { queryText, constant } = query;
+  const { calendarId } = query;
 
   return (
     <div className="gf-form">
-      <InlineField label="Constant">
-        <Input onChange={onConstantChange} value={constant} width={8} type="number" step="0.1" />
-      </InlineField>
-      <InlineField label="Query Text" labelWidth={16} tooltip="Not used yet">
-        <Input onChange={onQueryTextChange} value={queryText || ''} />
+      <InlineField label="Calender ID" labelWidth={16}>
+        <Input onChange={onCalendarIdChange} value={calendarId || ''} />
       </InlineField>
     </div>
   );
--- src/datasource.ts
+++ src/datasource.ts
@@ -1,14 +1,164 @@
-import { DataSourceInstanceSettings, CoreApp } from '@grafana/data';
-import { DataSourceWithBackend } from '@grafana/runtime';
+import {
+  DataSourceApi,
+  DataQueryRequest,
+  DataQueryResponse,
+  DataSourceInstanceSettings,
+  TestDataSourceResponse,
+  FieldType,
+  MutableDataFrame,
+} from '@grafana/data';
+import { GoogleCalendarQuery, GoogleCalendarDataSourceOptions } from './types';
+import _ from 'lodash';
+import { parse, parseISO } from 'date-fns';
 
-import { MyQuery, MyDataSourceOptions, DEFAULT_QUERY } from './types';
+export class DataSource extends DataSourceApi<GoogleCalendarQuery, GoogleCalendarDataSourceOptions> {
+  loaded: boolean;
+  initialized: boolean;
+  clientId: string;
 
-export class DataSource extends DataSourceWithBackend<MyQuery, MyDataSourceOptions> {
-  constructor(instanceSettings: DataSourceInstanceSettings<MyDataSourceOptions>) {
+  constructor(instanceSettings: DataSourceInstanceSettings<GoogleCalendarDataSourceOptions>) {
     super(instanceSettings);
+    this.loaded = false;
+    this.initialized = false;
+    this.clientId = instanceSettings.jsonData.clientId || '';
   }
 
-  getDefaultQuery(_: CoreApp): Partial<MyQuery> {
-    return DEFAULT_QUERY;
+  async loadScript(url: string) {
+    const script = document.createElement("script");
+    script.type = "text/javascript";
+    script.src = url;
+    document.getElementsByTagName("head")[0].appendChild(script);
+    return new Promise(resolve => {
+      script.onload = resolve;
+    })
+  }
+
+  async load() {
+    if (this.loaded) {
+      return;
+    }
+
+    await this.loadScript("https://accounts.google.com/gsi/client");
+    await this.loadScript("https://apis.google.com/js/api.js");
+
+    this.loaded = true;
+  }
+
+  async testDatasource(): Promise<TestDataSourceResponse> {
+    try {
+      await this.initialize();
+      return { status: 'success', message: 'Data source is working' };
+    } catch (error: any) {
+      console.error(error);
+      return { status: 'error', message: error.message };
+    }
+  }
+
+  async initialize() {
+    if (this.initialized) {
+      return;
+    }
+
+    await this.load();
+    await new Promise((resolve, reject) => {
+      gapi.load('client', { callback: resolve, onerror: reject });
+    });
+    await gapi.client.init({}).then(() => {
+      gapi.client.load('https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest');
+    });
+    const client: any = await new Promise((resolve, reject) => {
+      /* @ts-ignore */
+      resolve(google.accounts.oauth2.initTokenClient({
+        client_id: this.clientId,
+        scope: 'https://www.googleapis.com/auth/calendar.readonly',
+        prompt: 'consent',
+        callback: '',
+      }));
+    });
+    await new Promise((resolve, reject) => {
+      client.callback = (resp: any) => {
+        if (resp.error !== undefined) {
+          reject(resp);
+        } else {
+          gapi.client.getToken();
+          resolve(resp);
+        }
+      };
+      client.requestAccessToken();
+    });
+
+    this.initialized = true;
+  }
+
+  async query(options: DataQueryRequest<GoogleCalendarQuery>): Promise<DataQueryResponse> {
+    await this.initialize();
+    const eventList: any = await Promise.all(
+      options.targets
+        .filter(t => !t.hide && t.calendarId)
+        .map(async t => {
+          const params = {
+            calendarId: t.calendarId,
+            timeMin: options.range.from.toISOString(),
+            timeMax: options.range.to.toISOString(),
+            orderBy: 'startTime',
+            showDeleted: false,
+            singleEvents: true,
+            maxResults: 250,
+          };
+
+          const events = await this.getEvents(params);
+          /* @ts-ignore */
+          events.refId = t.refId;
+          events.sort((a, b) => {
+            return this.parseTime(a.start) > this.parseTime(b.start) ? 1 : -1;
+          });
+
+          return events;
+        })
+    );
+
+    const data = eventList.map((events: any) => {
+      const frame = new MutableDataFrame({
+        refId: events.refId,
+        fields: [
+          { name: 'startTime', type: FieldType.time },
+          { name: 'endTime', type: FieldType.time },
+          { name: 'summary', type: FieldType.string },
+          { name: 'displayName', type: FieldType.string },
+          { name: 'description', type: FieldType.string },
+        ],
+      });
+
+      events.forEach((event: any) => {
+        const start = this.parseTime(event.start);
+        const end = this.parseTime(event.end);
+        frame.add({
+          startTime: start,
+          endTime: end,
+          summary: event.summary,
+          displayName: event.organizer.displayName,
+          description: event.description || '',
+        })
+      });
+
+      return frame;
+    });
+
+    return { data };
+  }
+
+  async getEvents(params: any): Promise<gapi.client.calendar.Event[]> {
+    const response = await gapi.client.calendar.events.list(params);
+    return response.result.items;
+  }
+
+  parseTime(a: any): Date {
+    if (a.dateTime) {
+      return parseISO(a.dateTime.slice(0, 19));
+    } else if (a.date) {
+      return parse(a.date, 'yyyy-mm-dd', new Date());
+    } else {
+      throw new Error('Invalid time format');
+    }
   }
 }
