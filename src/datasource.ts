a4bf9f2c04154387a260ee434fc2303840c540e8
Mitsuhiro Tanda <mitsuhiro.tanda@gmail.com>
2024-05-30 09:35:19 +0900

    rewrite metricFindQuery function

--- src/datasource.ts
+++ src/datasource.ts
@@ -6,7 +6,9 @@ import {
   TestDataSourceResponse,
   FieldType,
   MutableDataFrame,
+  MetricFindValue,
 } from '@grafana/data';
+import { getTemplateSrv } from '@grafana/runtime';
 import { GoogleCalendarQuery, GoogleCalendarDataSourceOptions } from './types';
 import _ from 'lodash';
 import { parse, parseISO } from 'date-fns';
@@ -145,6 +147,117 @@ export class DataSource extends DataSourceApi<GoogleCalendarQuery, GoogleCalenda
     });
 
     return { data };
+  }
+
+  async metricFindQuery(query: any, options: any): Promise<MetricFindValue[]> {
+    await this.initialize();
+    const templateSrv = getTemplateSrv();
+    const fromRaw = parseInt(templateSrv.replace('$__from'), 10);
+    const from = new Date(fromRaw).toISOString();
+    const toRaw = parseInt(templateSrv.replace('$__to'), 10);
+    const to = new Date(toRaw).toISOString();
+    const eventsQuery = query.match(/^events\((([^,]+), *)?([^,]+), *([^,]+)\)/);
+    if (eventsQuery) {
+      const calendarId = eventsQuery[2];
+      const fieldPath = eventsQuery[3];
+      const filter = eventsQuery[4];
+      const params = {
+        calendarId: calendarId,
+        timeMin: from,
+        timeMax: to,
+        orderBy: 'startTime',
+        q: filter,
+        showDeleted: false,
+        singleEvents: true,
+        maxResults: 250,
+      };
+      return this.getEvents(params).then(events => {
+        return Promise.all(
+          events.map(event => {
+            return { text: _.get(event, fieldPath) };
+          })
+        );
+      });
+    }
+
+    const startEndQuery = query.match(/^(start|end)\((([^,]+), *)?([^,]+), *([^,]+), *([^,]+)\)/);
+    if (startEndQuery) {
+      const key = startEndQuery[1] === 'start' ? 'start' : 'end';
+      const calendarId = startEndQuery[3];
+      const format = startEndQuery[4];
+      const offset = parseInt(startEndQuery[5], 10);
+      const filter = startEndQuery[6];
+      const params = {
+        calendarId: calendarId,
+        timeMin: from,
+        timeMax: to,
+        orderBy: 'startTime',
+        q: filter,
+        showDeleted: false,
+        singleEvents: true,
+        maxResults: 250,
+      };
+      const events = await this.getEvents(params);
+      events.sort((a, b) => {
+        return this.parseTime(a.start) > this.parseTime(b.start) ? 1 : -1;
+      });
+      const lastIndex = 0;
+      const index = lastIndex - offset;
+      if (index < 0 || index >= events.length) {
+        return [];
+      }
+      let date: any = this.parseTime(events[index][key]);
+      if (format === 'offset' || format === '-offset') {
+        date = Math.floor((toRaw - date) / 1000);
+        if (format === 'offset') {
+          date = -date;
+        }
+        date = date + 's';
+      } else {
+        date = date.format(format);
+      }
+      return [{ text: date }];
+    }
+
+    const rangeQuery = query.match(/^range\((([^,]+), *)?([^,]+), *([^,]+), *([^,]+)\)/);
+    if (rangeQuery) {
+      const calendarId = rangeQuery[2];
+      const format = rangeQuery[3];
+      const offset = parseInt(rangeQuery[4], 10);
+      const filter = rangeQuery[5];
+      const params = {
+        calendarId: calendarId,
+        timeMin: from,
+        timeMax: to,
+        orderBy: 'startTime',
+        q: filter,
+        showDeleted: false,
+        singleEvents: true,
+        maxResults: 250,
+      };
+      return this.getEvents(params).then(events => {
+        events.sort((a, b) => {
+          return this.parseTime(a.start) > this.parseTime(b.start) ? 1 : -1;
+        });
+        const lastIndex = 0;
+        const index = lastIndex - offset;
+        if (index < 0 || index >= events.length) {
+          return [];
+        }
+        const end = this.parseTime(events[index].end);
+        const start = this.parseTime(events[index].start);
+        let range: any;
+        if (format === 'offset' || format === '-offset') {
+          range = Math.floor((end.valueOf() - start.valueOf()) / 1000);
+          if (format === 'offset') {
+            range = -range;
+          }
+        }
+        return [{ text: range + 's' }];
+      });
+    }
+
+    return Promise.reject(new Error('Invalid query'));
   }
 
   async getEvents(params: any): Promise<gapi.client.calendar.Event[]> {
